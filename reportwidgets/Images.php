b49c6b35aaa1eafac79fb7bb849b9f908d3dc60b
Szabó Gergő <gergo85@gmail.com>
2016-04-12 11:31:02 +0200

    Minor typo

--- reportwidgets/Images.php
+++ reportwidgets/Images.php
@@ -24,13 +24,31 @@ class Images extends ReportWidgetBase
                 'title'             => 'indikator.backend::lang.properties.category',
                 'default'           => '',
                 'type'              => 'dropdown',
-                'options'           => ['' => Lang::get('indikator.backend::lang.category.all'), 'abstract' => Lang::get('indikator.backend::lang.category.abstract'), 'animals' => Lang::get('indikator.backend::lang.category.animals'), 'business' => Lang::get('indikator.backend::lang.category.business'), 'cats' => Lang::get('indikator.backend::lang.category.cats'), 'city' => Lang::get('indikator.backend::lang.category.city'), 'food' => Lang::get('indikator.backend::lang.category.food'), 'nightlife' => Lang::get('indikator.backend::lang.category.nightlife'), 'fashion' => Lang::get('indikator.backend::lang.category.fashion'), 'people' => Lang::get('indikator.backend::lang.category.people'), 'nature' => Lang::get('indikator.backend::lang.category.nature'), 'sports' => Lang::get('indikator.backend::lang.category.sports'), 'technics' => Lang::get('indikator.backend::lang.category.technics'), 'transport' => Lang::get('indikator.backend::lang.category.transport')]
+                'options'           => [
+                    ''          => Lang::get('indikator.backend::lang.category.all'),
+                    'abstract'  => Lang::get('indikator.backend::lang.category.abstract'),
+                    'animals'   => Lang::get('indikator.backend::lang.category.animals'),
+                    'business'  => Lang::get('indikator.backend::lang.category.business'),
+                    'cats'      => Lang::get('indikator.backend::lang.category.cats'),
+                    'city'      => Lang::get('indikator.backend::lang.category.city'),
+                    'food'      => Lang::get('indikator.backend::lang.category.food'),
+                    'nightlife' => Lang::get('indikator.backend::lang.category.nightlife'),
+                    'fashion'   => Lang::get('indikator.backend::lang.category.fashion'),
+                    'people'    => Lang::get('indikator.backend::lang.category.people'),
+                    'nature'    => Lang::get('indikator.backend::lang.category.nature'),
+                    'sports'    => Lang::get('indikator.backend::lang.category.sports'),
+                    'technics'  => Lang::get('indikator.backend::lang.category.technics'),
+                    'transport' => Lang::get('indikator.backend::lang.category.transport')
+                ]
             ],
             'type' => [
                 'title'             => 'indikator.backend::lang.properties.type',
                 'default'           => 'simple',
                 'type'              => 'dropdown',
-                'options'           => ['simple' => Lang::get('indikator.backend::lang.widgets.images.simple'), 'slideshow' => Lang::get('indikator.backend::lang.widgets.images.slideshow')]
+                'options'           => [
+                    'simple'    => Lang::get('indikator.backend::lang.widgets.images.simple'),
+                    'slideshow' => Lang::get('indikator.backend::lang.widgets.images.slideshow')
+                ]
             ],
             'number' => [
                 'title'             => 'indikator.backend::lang.properties.number',
